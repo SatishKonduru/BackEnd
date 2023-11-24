f4249d65317144437d19250f09ecafb2770c2404
satish <satishkonduru0225@gmail.com>
2023-11-24 09:17:07 +0530

    authorization set as key 'autherization'

--- services/authentication.js
+++ services/authentication.js
@@ -3,8 +3,11 @@ const jwt = require('jsonwebtoken')
 
 
 function authenticateToken(req, res, next){
-    const authHeader = req.headers['autherization']
+    console.log("Request Data in Authentication process: ", req.headers)
+    const authHeader = req.headers['authorization']
+    console.log("authHeader: ", authHeader)
     const token = authHeader && authHeader.split(' ')[1]
+    console.log("Token: ", token)
     if(token == null){
         return res.sendStatus(401);
     }
